e24d7c11151bf70f7372f9c0762c0972d6e71a92
PenguinCabinet <kage2.kage22@gmail.com>
2024-08-28 20:27:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<div align="center">
+
+# 📘 nikki.ps1
+
+![img](/img/output.png)
+
+その日のテキストファイルを自動作成して、開く
+
+<br>
+<br>
+</div>
+
+## 🔨 使い方
+1. [nikki.ps1]()をダウンロードして、好きな場所においてください
+2. nikki.ps1を編集して、`$nikki_dir_path`を日記を置きたいフォルダのPATHにしてください
+3. nikki.ps1を実行すると、自動的にその日のテキストファイル(例:`日記\2024年4月1日.txt`)が作られ、既存のテキストエディタで開かれます
+
+## 🎫 LICENSE
+
+[MIT](./LICENSE)
+
+## ✍ Author
+
+[PenguinCabinet](https://github.com/PenguinCabinet)
---
+++ nikki.ps1
@@ -0,0 +1,23 @@
+﻿# CONFIG START
+$nikki_dir_path="日記"
+$nikki_format="yyyy年MM月dd日"
+$nikki_extension="txt"
+# CONFIG END
+
+
+
+if((Test-Path $nikki_dir_path) -eq "True"){
+ 	
+}else{
+	New-Item $nikki_dir_path -ItemType Directory
+}
+
+$today_nikki_path=(Join-Path $nikki_dir_path ((Get-Date -Format $nikki_format)+"."+$nikki_extension))
+
+if((Test-Path $today_nikki_path) -eq "True"){
+ 	
+}else{
+	New-Item $today_nikki_path
+}
+
+Invoke-Item $today_nikki_path
